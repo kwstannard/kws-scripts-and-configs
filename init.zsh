0506691972057002931c153c1ab61793caaf8217
Kelly Stannard <kelly.stannard@lendkey.com>
2014-12-18 17:40:14 -0500

    moved the gemdir file back out of lk

--- init.zsh
+++ init.zsh
@@ -2,5 +2,5 @@ source ~/scripts/init.sh
 
 for f in ~/scripts/zsh/*.zsh; do echo $f; source $f; done
 
-eval $(ruby ~/Sites/scripts/dev_machine/lib/generate_gem_dir_env_functions.rb)
+eval $(ruby ~/scripts/shell_scripts/generate_gem_dir_env_functions.rb)
 set_all_gem_dirs
