45141c0d79a3bc2e04530cd5a31f98f9e3e88888
Kelly Stannard <kelly.stannard@lendkey.com>
2015-01-27 09:56:03 -0500

    dont output blank lines if nothing found in a directory

--- shell_scripts/seek/seek_all_projects.rb
+++ shell_scripts/seek/seek_all_projects.rb
@@ -7,7 +7,8 @@ puts "seeking for '#{word}' with args #{args} under path '#{path}'"
 
 sites_path = '/Users/kstannard/Sites/'
 Dir["#{sites_path}{apps,gems}/*"].each do |d|
-  puts %x[~/scripts/bin/project_grep '#{word}' #{args.join(" ")} #{d}/#{path}].gsub(sites_path, '')
+  finds = %x[~/scripts/bin/project_grep '#{word}' #{args.join(" ")} #{d}/#{path}].gsub(sites_path, '')
+  puts finds if finds.strip.size > 0
 end
 #puts %x[~/scripts/bin/project_grep '#{word}' #{args.join(" ")} #{sites_path}gems/duzica/#{path}].gsub(sites_path, '')
 #puts %x[~/scripts/bin/project_grep '#{word}' #{args.join(" ")} #{sites_path}gems/sakura/#{path}].gsub(sites_path, '')
