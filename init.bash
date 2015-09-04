87b7b0ac1b8b45a0474c2d2bb78c1742c38a7a83
Kelly Stannard <kelly.stannard@caspersleep.com>
2015-09-04 15:25:07 -0400

    history improvements

--- init.bash
+++ init.bash
@@ -1,3 +1,10 @@
 source ~/scripts/init.sh
-
 for f in ~/scripts/bash/*.bash; do echo $f; source $f; done
+
+# Avoid duplicates
+export HISTCONTROL=ignoredups:erasedups
+# When the shell exits, append to the history file instead of overwriting it
+shopt -s histappend
+
+# After each command, append to the history file and reread it
+export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
