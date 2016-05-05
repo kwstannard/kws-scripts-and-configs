bd84c3f74bb9cd30f27e261b207c529d783149b6
Kelly Stannard <kelly.stannard@caspersleep.com>
2016-05-05 11:42:50 -0400

    shell init changes
    - add sbin to path
    - disable spring by default

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 for f in ~/scripts/shell_scripts{,/**}/*.sh; do echo $f; source $f; done
 
+export PATH="/usr/local/sbin:$PATH"
 export PATH="$HOME/scripts/bin:$PATH"
 export PATH="/usr/local/bin:$PATH"
 export PATH="/usr/local/heroku/bin:$PATH"
 export PATH="$HOME/scripts/pull_all_projects:$PATH"
+
+export DISABLE_SPRING=1
