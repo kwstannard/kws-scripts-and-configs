2ba86d737b64b452d338578af88fd8b688f55608
Kelly Stannard <kelly.stannard@caspersleep.com>
2016-06-02 11:39:30 -0400

    remove old script

--- bash/clean_whitespace.bash
+++
@@ -1,9 +0,0 @@
-clean_whitespace(){
-  for f in ~/Sites/apps/*; do
-    ~/scripty_scripts/clean_whitespace $f
-  done
-
-  clean_whitespace ~/Sites/gems/sakura
-  clean_whitespace ~/Sites/gems/duzica
-}
-
