bcc9dc091a213c9d2028fcf8241cb46e42406ff3
Kelly Stannard <kelly.stannard@caspersleep.com>
2016-07-10 16:05:44 -0400

    fix command prompt

--- bash/prompt_script.bash
+++ bash/prompt_script.bash
@@ -1,5 +1,7 @@
 function setbashprompt() {
   PS1="$(ruby $HOME/scripts/bash/prompt_runner.rb)\n>> "
+# After each command, append to the history file and reread it
+  history -a; history -c; history -r
 }
 
 PROMPT_COMMAND=setbashprompt
--- init.bash
+++ init.bash
@@ -5,6 +5,3 @@ for f in ~/scripts/bash/*.bash; do echo $f; source $f; done
 export HISTCONTROL=ignoredups:erasedups
 # When the shell exits, append to the history file instead of overwriting it
 shopt -s histappend
-
-# After each command, append to the history file and reread it
-export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
