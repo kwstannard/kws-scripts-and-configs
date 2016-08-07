9ccb280ccbbd2389820252675146b9e4c003d323
Bob Dole <bobdole@localhost.localdomain>
2016-08-06 23:24:21 -0400

    use different git config based on system

--- configs_setup.sh
+++ configs_setup.sh
@@ -1,11 +1,13 @@
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+system_type="$(uname -s)"
+echo $system_type
 
 ln -s $dir/configs/vimrc.vim ~/.vimrc
 mkdir ~/.config/nvim
 ln -s $dir/configs/nvimrc.vim ~/.config/nvim/init.vim
 
 ln -s $dir/configs/vimperatorrc ~/.vimperatorrc
-ln -s $dir/configs/gitconfig ~/.gitconfig
+ln -s $dir/configs/$system_type/gitconfig ~/.gitconfig
 ln -s $dir/configs/inputrc ~/.inputrc
 ln -s $dir/configs/pryrc ~/.pryrc
 ln -s $dir/configs/psqlrc ~/.psqlrc
@@ -13,7 +15,7 @@ ln -s $dir/configs/psqlrc ~/.psqlrc
 rm ~/.bashrc
 ln -s $dir/configs/bashrc ~/.bashrc
 
-case "$(uname -s)" in
+case $system_type in
   "Linux")
     ;;
   "Darwin")
