f44c91d78a7b60f5664640db64738cac60b70174
Bob Dole <bobdole@localhost.localdomain>
2016-08-06 23:20:37 -0400

    git configs

--- configs_setup.sh
+++ configs_setup.sh
@@ -4,11 +4,19 @@ ln -s $dir/configs/vimrc.vim ~/.vimrc
 mkdir ~/.config/nvim
 ln -s $dir/configs/nvimrc.vim ~/.config/nvim/init.vim
 
+ln -s $dir/configs/vimperatorrc ~/.vimperatorrc
+ln -s $dir/configs/gitconfig ~/.gitconfig
+ln -s $dir/configs/inputrc ~/.inputrc
+ln -s $dir/configs/pryrc ~/.pryrc
+ln -s $dir/configs/psqlrc ~/.psqlrc
+
+rm ~/.bashrc
+ln -s $dir/configs/bashrc ~/.bashrc
+
 case "$(uname -s)" in
   "Linux")
-    echo hi
     ;;
   "Darwin")
-    echo oh!
+    ln -s $dir/configs/mac/bash_profile ~/.bash_profile
     ;;
 esac
