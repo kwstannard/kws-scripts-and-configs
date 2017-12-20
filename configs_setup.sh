012655e4e1dc937408b631e5f365992230d62c9f
Kelly Wolf Stannard <kstannard@mdsol.com>
2017-12-20 15:26:42 -0500

    checkin

--- configs_setup.sh
+++ configs_setup.sh
@@ -10,12 +10,14 @@ ln -fs $dir/configs/plug.vim ~/.config/nvim/autoload/plug.vim
 ln -fs $dir/configs/plug.vim ~/.vim/autoload/plug.vim
 
 ln -fs $dir/configs/vimperatorrc ~/.vimperatorrc
-ln -fs $dir/configs/$system_type/gitconfig ~/.gitconfig
 ln -fs $dir/configs/pryrc ~/.pryrc
 ln -fs $dir/configs/psqlrc ~/.psqlrc
+ln -fs $dir/configs/tmux.conf ~/.tmux.conf
 
 ln -fs $dir/configs/$system_type/bashrc ~/.bashrc
 ln -fs $dir/configs/$system_type/bash_profile ~/.bash_profile
+ln -fs $dir/configs/$system_type/inputrc ~/.inputrc
+ln -fs $dir/configs/$system_type/gitconfig ~/.gitconfig
 
 ln -fs $dir/git_template ~/.git_template
 
@@ -23,10 +25,8 @@ case $system_type in
   "Linux")
     ln -fs $dir/configs/Linux/i3config ~/.config/i3/config
     ln -fs /bin/grep ~/bin/ggrep
-    ln -fs $dir/configs/Linux/inputrc ~/.inputrc
     ;;
   "Darwin")
-    ln -fs $dir/configs/Darwin/inputrc ~/.inputrc
     ln -fs $dir/configs/Darwin/khdrc ~/.khdrc
     ;;
 esac
