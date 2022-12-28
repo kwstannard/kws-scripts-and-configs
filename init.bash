8f77ce348a06c0e1e15ec8cead9e3d6c588568eb
Kelly Stannard <kwstannard@gmail.com>
2022-12-28 14:15:13 -0500

    checkin

--- init.bash
+++ init.bash
@@ -1,7 +1,7 @@
 source ~/scripts/init.sh
-for f in ~/scripts/bash/*.bash; do echo $f; source $f; done
+for f in ~/scripts/bash/*.bash; do source $f; done
 
 # Avoid duplicates
-export HISTCONTROL=ignoredups:erasedups
+export HISTCONTROL=erasedups
 # When the shell exits, append to the history file instead of overwriting it
 shopt -s histappend
--- init.sh
+++ init.sh
@@ -1,13 +1,14 @@
+echo init.sh!
 PATH="/usr/local/sbin:$PATH"
 PATH="/usr/local/opt/postgresql@9.6/bin:$PATH"
 
 HOMEBREW_PREFIX="/usr/local"
 
-for f in ~/scripts/shell_scripts/*.sh; do echo $f; source $f; done
+for f in ~/scripts/shell_scripts/*.sh; do source $f; done
 PATH="$HOME/scripts/bin:$PATH"
 
 DISABLE_SPRING=1
-HISTSIZE=15000
+HISTSIZE=500000
 HISTFILESIZE=1000000
 
 NVM_DIR="$HOME/.nvm"
