304d68135c254afccbeee96cf2833d6f5c7dc797
Kelly Wolf Stannard <kwstannard@gmail.com>
2025-10-07 13:19:13 -0400

    checkin

--- dot-config/fish/conf.d/chruby.fish
+++ dot-config/fish/conf.d/chruby.fish
@@ -1,3 +1,6 @@
-for f in /opt/homebrew/share/fish/vendor_functions.d/*; source $f; end
+source $(brew --prefix chruby-fish)/share/fish/vendor_functions.d/chruby_reset.fish
+source $(brew --prefix chruby-fish)/share/fish/vendor_functions.d/chruby_use.fish
+source $(brew --prefix chruby-fish)/share/fish/vendor_functions.d/chruby.fish
+source $(brew --prefix chruby-fish)/share/fish/vendor_conf.d/chruby_auto.fish
 
-  chruby scripting
+chruby scripting
